5287e25089cc4dcd5bb53e56b705cb043712e879
fpanos <fpanos@triarail.com>
2023-05-29 17:57:54 +0200

    may2905

--- components/FichaVehiculo.jsx
+++ components/FichaVehiculo.jsx
@@ -4,13 +4,13 @@ import Image from "next/image";
 
 export default function FichaVehiculo ({vehiculo}) {
   return(
-  <div className='my-2 pb-4 border border-gray-200 rounded-lg shadow h-[24rem] w-72 mx-auto'>
+  <div className='my-2 pb-4 border border-gray-200 rounded-lg shadow h-[21rem] w-72 mx-auto'>
     <div className=''>
       <Image src = {`/imagenes/vehiculos/${vehiculo.tipo.imagen}`} alt = '' height = {100} width = {180} className='rounded-t-lg object-fill w-72'/>
       <div className='flex flex-col justify-between mb-2'>
-        <div className="text-center mt-2 p-1 text-xl text-gray-800 truncate">{vehiculo.num_uic}</div>
-        <div className="text-center text-xl text-gray-500 truncate">{vehiculo.keeper}</div>
-        <div className="text-center mb-2 text-xl text-gray-500">{vehiculo.tipo.descripcion}</div>
+        <div className="text-center mt-2 p-1 text-2xl text-gray-800 truncate">{vehiculo.num_uic}</div>
+        <div className="text-center text-2xl text-gray-500 truncate">{vehiculo.keeper}</div>
+        <div className="text-center mb-2 text-2xl text-gray-500">{vehiculo.tipo.descripcion}</div>
       </div>
     </div>
     <div className="flex justify-between my-2 mx-12 px-2 py-1 rounded-full bg-gray-100">
